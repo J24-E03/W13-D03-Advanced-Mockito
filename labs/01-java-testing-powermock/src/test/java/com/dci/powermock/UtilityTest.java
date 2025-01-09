b9f4d57fb7d5e58a8b7ff3405e70746ad2084651
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2025-01-09 11:19:54 +0100

    Implement week 13 day 3 lab 1
    Refactor `UtilityTest` by replacing `PowerMock` with `Mockito`.

--- labs/01-java-testing-powermock/src/test/java/com/dci/powermock/UtilityTest.java
+++ labs/01-java-testing-powermock/src/test/java/com/dci/powermock/UtilityTest.java
@@ -1,48 +1,42 @@
 package com.dci.powermock;
 
-import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(Utility.class)
-public class UtilityTest {
+class UtilityTest {
 
     @Test
-    public void verifySayHelloStaticMethod() {
+    void verifySayHelloStaticMethod() {
         String name = "Robert Bosch";
 
-        PowerMockito.mockStatic(Utility.class);
-        PowerMockito.when(Utility.sayHello(name)).thenReturn("Hello! Robert Bosch");
+        try (MockedStatic<Utility> mockedStatic = mockStatic(Utility.class)) {
+            mockedStatic.when(() -> Utility.sayHello(name)).thenReturn("Hello! Robert Bosch");
 
-        String actualMessage = Utility.sayHello(name);
-        assertEquals("Hello! Robert Bosch", actualMessage);
+            String actualMessage = Utility.sayHello(name);
+            assertEquals("Hello! Robert Bosch", actualMessage);
+        }
     }
 
     @Test
-    public void verifyGetGreetingMessagePrivateMethod() throws Exception {
+    void verifyGetGreetingMessagePrivateMethod() {
         String name = "Robert Bosch";
 
-        Utility mock =PowerMockito.spy(new Utility());
-        PowerMockito.doReturn("Good Morning! Robert Bosch").when(mock, "getMessage", name);
+        Utility utility = spy(new Utility());
+        doReturn("Good Morning! Robert Bosch").when(utility).getMessage(name);
 
-
-        String actualMessage = mock.getGreetingMessage(name);
+        String actualMessage = utility.getGreetingMessage(name);
         assertEquals("Good Morning! Robert Bosch", actualMessage);
     }
 
     @Test
-    public void verifyGetGreetingMessageFinalMethod() throws Exception {
+    void verifyGetGreetingMessageFinalMethod() {
         String name = "Robert Bosch";
 
-        Utility utility = PowerMockito.mock(Utility.class);
-        PowerMockito.when(utility.getCongratsMessage(name)).thenReturn("Congratulations! Robert Bosch");
-
+        Utility utility = mock(Utility.class);
+        when(utility.getCongratsMessage(name)).thenReturn("Congratulations! Robert Bosch");
 
         String actualMessage = utility.getCongratsMessage(name);
         assertEquals("Congratulations! Robert Bosch", actualMessage);
