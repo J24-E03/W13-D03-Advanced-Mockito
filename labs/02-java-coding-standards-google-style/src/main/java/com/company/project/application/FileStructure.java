8834a5df3fa6384be251ae933fe155b9623e046d
omarakamal <omaraakamal@gmail.com>
2025-01-09 08:47:08 +0300

    starter code

---
+++ labs/02-java-coding-standards-google-style/src/main/java/com/company/project/application/FileStructure.java
@@ -0,0 +1,15 @@
+package com.company.project.application;
+
+import com.company.project.application.configuration.*;
+
+/**
+ * Checks if the given date fits the current month. Returns true if that is the case.
+ */
+public class FileStructure {
+  private final FileStructureConfig fileStructureConfig = new FileStructureConfig();
+}
+
+class FileStructureProperties {
+
+  public static final String ENCODING = "UTF-8";
+}
