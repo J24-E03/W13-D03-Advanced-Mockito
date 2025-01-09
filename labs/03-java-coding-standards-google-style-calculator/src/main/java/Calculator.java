56025fbff72031f718add64dbdd555f4730f466d
Marcel Saegebarth <marcel.saegebarth@dci-student.org>
2025-01-09 10:47:15 +0100

    Implement week 13 day 3 lab 3

--- labs/03-java-coding-standards-google-style-calculator/src/main/java/Calculator.java
+++ labs/03-java-coding-standards-google-style-calculator/src/main/java/Calculator.java
@@ -1,59 +1,96 @@
 import java.util.Scanner;
 
+/**
+ * A simple calculator that can perform addition, subtraction, multiplication and division.
+ * The user is prompted to enter two numbers and the operation they want to perform.
+ * The result is then displayed to the user.
+ */
 public class Calculator {
-  public static void main(String[] args)
-  {
+  /**
+   * Main entry point of the program.
+   *
+   * @param args The command line arguments.
+   */
+  public static void main(String[] args) {
     System.out.println("Enter first and second number:");
-    Scanner INPUT= new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
-    int num1,num2;
-    num1 = INPUT.nextInt();
-    num2 = INPUT.nextInt();
+    int num1;
+    int num2;
+    num1 = input.nextInt();
+    num2 = input.nextInt();
 
-    System.out.println("Enter the operation: + for addition, - for subtraction, * for multiplication or / for division:");
-    String calc_operation;
-    calc_operation = INPUT.next();
+    StringBuilder sb = new StringBuilder();
+    sb
+        .append("Enter the operation: ")
+        .append("+ for addition, ")
+        .append("- for subtraction, ")
+        .append("* for multiplication or ")
+        .append("/ for division:");
+    System.out.println(sb);
+    String calcOperation;
+    calcOperation = input.next();
 
-    switch (calc_operation){
+    switch (calcOperation) {
       case "+":
-        System.out.println(add( num1,num2));
+        System.out.println(add(num1, num2));
         break;
       case "-":
-        System.out.println(sub( num1,num2));
+        System.out.println(sub(num1, num2));
         break;
       case "*":
-        System.out.println(Multiply( num1,num2));
+        System.out.println(multiply(num1, num2));
         break;
       case "/":
-        System.out.println(divide_numbers( num1,num2));
+        System.out.println(divideNumbers(num1, num2));
         break;
       default:
         System.out.println("No operation selected");
     }
   }
 
-  public static int add(int x, int y)
-  {
-    int result = x + y;
-    return result;
+  /**
+   * Adds two numbers.
+   *
+   * @param x The first number.
+   * @param y The second number.
+   * @return The sum of the two numbers.
+   */
+  public static int add(int x, int y) {
+    return x + y;
   }
 
-  public static int sub(int x, int y)
-  {
-    int result = x-y;
-    return result;
+  /**
+   * Subtracts two numbers.
+   *
+   * @param x The first number.
+   * @param y The second number.
+   * @return The difference of the two numbers.
+   */
+  public static int sub(int x, int y) {
+    return x - y;
   }
 
-  public static int Multiply(int x, int y)
-  {
-    int result = x*y;
-    return result;
+  /**
+   * Multiplies two numbers.
+   *
+   * @param x The first number.
+   * @param y The second number.
+   * @return The product of the two numbers.
+   */
+  public static int multiply(int x, int y) {
+    return x * y;
   }
 
-  public static int divide_numbers(int x, int y)
-  {
-    int result = x/y;
-    return result;
+  /**
+   * Divides two numbers.
+   *
+   * @param x The first number.
+   * @param y The second number.
+   * @return The division of the two numbers.
+   */
+  public static int divideNumbers(int x, int y) {
+    return x / y;
   }
 
 }
