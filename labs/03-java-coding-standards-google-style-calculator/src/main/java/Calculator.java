5681fb13f80cf9e89baf96e6b9c4cd47e7a5232c
Scynaide <aron.turowski@dci-student.org>
2025-01-09 12:05:41 +0100

    adding some changes to the code

--- labs/03-java-coding-standards-google-style-calculator/src/main/java/Calculator.java
+++ labs/03-java-coding-standards-google-style-calculator/src/main/java/Calculator.java
@@ -4,56 +4,53 @@ public class Calculator {
   public static void main(String[] args)
   {
     System.out.println("Enter first and second number:");
-    Scanner INPUT= new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
     int num1,num2;
-    num1 = INPUT.nextInt();
-    num2 = INPUT.nextInt();
+    num1 = input.nextInt();
+    num2 = input.nextInt();
 
     System.out.println("Enter the operation: + for addition, - for subtraction, * for multiplication or / for division:");
     String calc_operation;
-    calc_operation = INPUT.next();
+    calc_operation = input.next();
 
     switch (calc_operation){
       case "+":
-        System.out.println(add( num1,num2));
+        System.out.println(add(num1,num2));
         break;
       case "-":
-        System.out.println(sub( num1,num2));
+        System.out.println(sub(num1,num2));
         break;
       case "*":
-        System.out.println(Multiply( num1,num2));
+        System.out.println(multiply(num1,num2));
         break;
       case "/":
-        System.out.println(divide_numbers( num1,num2));
+        if (num2 !=0){
+          System.out.println(divide(num1,num2));
+        } else {
+          System.out.println("You cannot divine by 0");
+        }
         break;
       default:
-        System.out.println("No operation selected");
+        System.out.println("Invalid selection");
     }
   }
 
-  public static int add(int x, int y)
-  {
-    int result = x + y;
-    return result;
+  public static int add(int x, int y){
+    return x + y;
   }
 
-  public static int sub(int x, int y)
-  {
-    int result = x-y;
-    return result;
+
+  public static int sub(int x, int y) {
+    return x - y;
   }
 
-  public static int Multiply(int x, int y)
-  {
-    int result = x*y;
-    return result;
+  public static int multiply(int x, int y) {
+    return x * y;
   }
 
-  public static int divide_numbers(int x, int y)
-  {
-    int result = x/y;
-    return result;
+  public static int divide(int x, int y) {
+    return x / y;
   }
 
 }
